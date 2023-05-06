3a37963e1045b6c982407c86a8851b2e5449f68b
Albert Tian <atian@berkeley.edu>
2023-05-06 12:26:19 -0700

    misc: switch api_url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "https://web-production-f5a8.up.railway.app"
-export const API_URL = "http://0.0.0.0:8000"
+export const API_URL = "https://web-production-f5a8.up.railway.app"
+// export const API_URL = "http://0.0.0.0:8000"
 
 
 // https://web-production-f5a8.up.railway.app/takehome/get_candidates_for_job/1
